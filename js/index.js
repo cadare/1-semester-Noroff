0306587f535354e745862ab265f2204243ea1343
khadar Adareh <cadare@gmail.com>
2022-02-07 14:55:13 +0300

    loaded up homepage and things related to it

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Hamburger meny
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+});
